bc4a47e35e02a677646de73835c9d139f564ceee
User <User@DESKTOP-73TAQI5>
2020-12-30 20:29:27 +0900

    Chapter04

---
+++ src/icehs/science/chapter04/DivisionEx.java
@@ -0,0 +1,17 @@
+package icehs.science.chapter04;
+
+public class DivisionEx {
+
+	public static void main(String[] args) {
+		int number1 = 10;
+		int number2 = 3;
+		
+		int divisionReasult = number1 / number2;
+		int remainderReasult = number1 % number2;
+		
+		System.out.println(number1 + "/" + number2);
+		System.out.println("³ª´°¼À °á°ú : " +divisionReasult);
+		System.out.println("³ª¸ÓÁö : " + remainderReasult);
+	}
+
+}
